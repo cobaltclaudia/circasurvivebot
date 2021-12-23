1ffd9cd65d8b44198d432943f636a61336101e81
claudia <ceruleancee@gmail.com>
2021-12-23 00:35:00 -0600

    Minor package change

--- src/main/java/CircaSurviveBot/App/CircaSurviveBotApp.java
+++ src/main/java/CircaSurviveBot/App/CircaSurviveBotApp.java
@@ -1,9 +1,12 @@
+package CircaSurviveBot.App;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CircaSurviveBotApp {
 
     public static void main(String[] args) {
-        //SpringApplication.run(CircaSurviveBotApp.class,args);
+        SpringApplication.run(CircaSurviveBotApp.class,args);
     }
 }
